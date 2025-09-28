535e0fb6aba8a8c8a7b11f710acf37e666f59421
hdju <kiribati1234@gmail.com>
2025-09-28 19:52:03 +0900

    first commit

---
+++ ManualZipSwiftUITests/ManualZipSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ManualZipSwiftUITests.swift
+//  ManualZipSwiftUITests
+//
+//  Created by hongdae on 9/28/25.
+//
+
+import Testing
+@testable import ManualZipSwiftUI
+
+struct ManualZipSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ManualZipSwiftUIUITests/ManualZipSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ManualZipSwiftUIUITestsLaunchTests.swift
+//  ManualZipSwiftUIUITests
+//
+//  Created by hongdae on 9/28/25.
+//
+
+import XCTest
+
+final class ManualZipSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
